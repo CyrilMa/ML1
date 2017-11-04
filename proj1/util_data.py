9e37de49215a1af2adcf80fd8860032f9c3c7113
CyrilAlari <cyril1996@gmail.com>
2017-11-04 13:20:23 +0200

    gini add

---
+++ proj1/util_data.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+
+
+class DataSet:
+    train_data = pd.read_csv('data/train.csv')
+    test_data = pd.read_csv('data/test.csv')
+    
+    def __init__(self):
+        return
+    
+    def get_training_set(self,n = None):
+        if(n):
+            return(self.train_data.sample(n))
+        else:
+            return(self.train_data)
+    
+    def get_testing_set(self, n = None):
+        if(n):
+            return(self.test_data.sample(n))
+        else:
+            return(self.test_data)
+    
+    def get_scaled_training_set(self, n_zeros, n_ones = None):
+        zeros = self.train_data[self.train_data['target']==0]
+        ones = self.train_data[self.train_data['target']==1]
+        
+        if(n_ones):
+            return(pd.concat([zeros.sample(n_zeros),ones.sample(n_ones)],axis=0))
+        
+        else:
+            return(pd.concat([zeros.sample(n_zeros),ones],axis=0))
+
+    
+    
